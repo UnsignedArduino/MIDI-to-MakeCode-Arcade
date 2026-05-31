b0a2628f14fd33d85f362d412ceb942c60035746
Cyrus Yiu <UnsignedArduino@outlook.com>
2026-05-31 19:49:26 -0400

    Can write to file for output now

--- src/main.py
+++ src/main.py
@@ -10,12 +10,14 @@ from converter.midi_to_song import convert_midi_to_song
 from utils.logger import create_logger, set_all_stdout_logger_levels
 
 parser = ArgumentParser(description="Convert a MIDI file to a MakeCode Arcade song.")
-parser.add_argument("--input", "-i", type=Path,
+parser.add_argument("--input", "-i", type=Path, required=True,
                     help="Input MIDI file.")
 parser.add_argument("--input-instrument-params", "-p", type=Path,
-                    help="Input instrument parameter mapping file.")
-# parser.add_argument("--output", "-o", type=Path,
-#                     help="Output TypeScript file path, otherwise will write to stdout.")
+                    required=True, help="Input instrument parameter mapping file.")
+parser.add_argument("--output", "-o", type=Path,
+                    help="Output TypeScript file path, otherwise will write to stdout. "
+                         "If specified, this will overwrite the file if it already "
+                         "exists, and the parent directories must exist.")
 parser.add_argument("--debug", action="store_const",
                     const=logging.DEBUG, default=logging.INFO,
                     help="Include debug messages. Defaults to info and "
@@ -26,13 +28,13 @@ set_all_stdout_logger_levels(args.debug)
 logger.debug(f"Received arguments: {args}")
 
 input_path = Path(args.input)
-logger.debug(f"Reading MIDI file from {input_path}")
+logger.info(f"Reading MIDI file from {input_path}")
 
 mid = MidiFile(input_path)
 logger.debug(f"Found {len(mid.tracks)} tracks, length of {mid.length}s")
 
 input_instrument_param_path = Path(args.input_instrument_params)
-logger.debug(f"Reading instrument parameter mapping from {input_instrument_param_path}")
+logger.info(f"Reading instrument parameter mapping from {input_instrument_param_path}")
 
 mapping = load_instrument_params(input_instrument_param_path.read_text())
 logger.debug(f"Mapped {len(mapping.melodic_instruments)} melodic instruments and "
@@ -40,5 +42,13 @@ logger.debug(f"Mapped {len(mapping.melodic_instruments)} melodic instruments and
 
 song = convert_midi_to_song(mid, mapping)
 h = encode_song_to_hex(song)
+final_output = f"hex`{h}`"
 logger.info("Finished converting MIDI file")
-print(f"hex`{h}`")
+
+output_path = Path(args.output) if args.output is not None else None
+if output_path is not None:
+    logger.info(f"Writing result to {output_path}")
+    output_path.write_text(final_output)
+else:
+    logger.info(f"Writing result to stdout")
+    print(final_output)
